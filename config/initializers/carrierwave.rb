dd68f768064e46a3c74f4dd6407564f01b7dd282
Tristan Hart <tristanhart@gmail.com>
2019-03-02 18:39:10 +1100

    Rename env variables for S3 bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,9 +18,9 @@ CarrierWave.configure do |config|
     }
   
     config.aws_credentials = {
-      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      region:            ENV.fetch('AWS_REGION'), # Required
+      access_key_id:     ENV.fetch('AWS_S3_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_S3_SECRET_ACCESS_KEY'),
+      region:            ENV.fetch('AWS_S3_REGION'), # Required
       stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
     }
   
